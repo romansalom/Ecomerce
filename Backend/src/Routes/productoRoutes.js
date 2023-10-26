758ecf4297b55aaa3649c9f67c02faa3ca7c5fce
romansalom <roman.salom12@gmail.com>
2023-10-26 15:21:50 -0300

    base de datos en render

--- Backend/src/Routes/productoRoutes.js
+++ Backend/src/Routes/productoRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Ruta para obtener todos los productos (GET)
 router.get('/todos', productoController.getAllProductos);
 
-// Ruta para crear un nuevo producto (POST)
+// Rutas para crear un nuevo producto (POST)
 router.post('/post', productoController.createProducto);
 
 //Ruta para agregar múltiples productos
