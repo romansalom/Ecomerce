c101cca9107a54e4759548dae3b57489499da1d4
romansalom <roman.salom12@gmail.com>
2023-10-26 02:09:08 -0300

    tabla principa;

--- my-project/src/componentes/Cards/cards.jsx
+++ my-project/src/componentes/Cards/cards.jsx
@@ -4,8 +4,9 @@ import axios from 'axios';
 function Cards() {
   const [pricingData, setPricingData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedMarca, setSelectedMarca] = useState(''); 
-  const [uniqueMarcas, setUniqueMarcas] = useState([]); 
+  const [selectedMarca, setSelectedMarca] = useState('');
+  const [uniqueMarcas, setUniqueMarcas] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     axios
@@ -57,92 +58,131 @@ function Cards() {
   .filter((product) =>
     selectedMarca ? product.marca === selectedMarca : true
   );
+  const openPreview = (product) => {
+    setSelectedProduct(product);
+  };
 
+  // Función para cerrar la vista previa
+  const closePreview = () => {
+    setSelectedProduct(null);
+  };
   return (
-    <div className="bg-white bg-gray">
-      <div className="mt-4">TODOS LOS PRODUCTOS</div>
+    <div className="font-mono bg-white bg-gray">
+      <div className="mt-4 text-2xl">TODOS LOS PRODUCTOS</div>
       <div className="container px-6 py-8 mx-auto">
-        <input
-          type="text"
-          placeholder="Buscar por nombre"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full p-2 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
-        />
-        <select
-          value={selectedMarca}
-          onChange={(e) => setSelectedMarca(e.target.value)}
-          className="w-full p-2 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
-        >
-          <option value="">Todas las marcas</option>
-          {uniqueMarcas.map((marca, index) => (
-            <option key={index} value={marca}>
-              {marca}
-            </option>
-          ))}
-        </select>
-        <div className="h-9"></div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Buscar por nombre"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-48 p-2 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
+          />
+        </div>
+        <div className="mb-4">
+          <select
+            value={selectedMarca}
+            onChange={(e) => setSelectedMarca(e.target.value)}
+            className="w-48 p-2 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-200"
+          >
+            <option value="">Todas las marcas</option>
+            {uniqueMarcas.map((marca, index) => (
+              <option key={index} value={marca}>
+                {marca}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="h-6"></div>
         {filteredPricingData.length === 0 ? (
           <p>No hay productos</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {filteredPricingData.map((pricing, index) => (
-              <div
-                key={index}
-                className="p-4 text-center bg-gray-100 border border-gray-300 rounded-lg shadow-xl hover:shadow-lg"
-              >
-                <div className="flex-shrink-0">
-                <img src={pricing.imageUrl} alt={pricing.name} className="w-32 h-32 mx-auto mb-4 rounded-full" />
-                  <h2 className="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-black uppercase rounded-lg bg-gray-00 dark:bg-gray">
-                    {pricing.name}
-                  </h2>
-                </div>
-                <div className="h-2"></div>
-                <div className="flex-shrink-0">
-                  <h3
-                    className="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-black uppercase rounded-lg bg-gray-00 dark:bg-gray"
-                    style={{ fontSize: '1.2em' }}
-                  >
-                    Precio: ${pricing.precio}
-                  </h3>
-                </div>
-                <div className="h-2"></div>
-                <ul className="flex-1 space-y-4">
-                  <li className="text-gray-500 dark:text-gray-400">Sabor: {pricing.flavor}</li>
-                  <li className="text-gray-500 dark:text-gray-400">Puffs: {pricing.puffs}</li>
-                  <li className="text-gray-500 dark:text-gray-400">Marca: {pricing.marca}</li>
-                </ul>
-                <div className="h-2"></div>
-                <div className="flex items-center justify-center space-x-4">
-                  <button
-                    onClick={() => decreaseQuantity(index)}
-                    className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-lg"
-                    disabled={pricing.quantity === 0}
-                  >
-                    <span style={{ fontSize: '1.25em' }}>-</span>
-                  </button>
-                  <span className="text-lg">{pricing.quantity}</span>
-                  <button
-                    onClick={() => increaseQuantity(index)}
-                    className="bg-green-500 hover:bg-green-600 text-white px-2 py-1 rounded-lg"
-                    disabled={pricing.quantity >= pricing.stock}
-                  >
-                    <span style={{ fontSize: '1.25em' }}>+</span>
-                  </button>
-                </div>
-                <div className="h-2"></div>
-                <button
-                  onClick={() => addToCart(pricing)}
-                  className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded-lg"
-                >
-                  Agregar al Carrito
-                </button>
-              </div>
-            ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {filteredPricingData.map((pricing, index) => (
+  <div
+    key={index}
+    className="p-3 transform scale-80 text-center bg-gray-100 border border-gray-300 rounded-lg shadow-xl hover:shadow-lg"
+  >
+    <div className="flex-shrink-0 mt-2">
+      <img src={pricing.imageUrl} alt={pricing.name} className="w-24 h-24 mx-auto mb-2 rounded-full" />
+      <h2 className="text-2xl font-semibold text-black mb-2">{pricing.name}</h2>
+    </div>
+    <div className="h-2"></div>
+    <div className="flex-shrink-0">
+      <h3 className="text-2xl font-semibold text-black">${pricing.precio}</h3>
+    </div>
+    <div className="h-2"></div>
+    <ul className="text-sm text-gray-600 space-y-2">
+      <li>Sabor: {pricing.flavor}</li>
+      <li>{pricing.puffs} Puffs</li>
+      <li>{pricing.marca}</li>
+    </ul>
+    <div className="h-2"></div>
+    <div className="flex items-center justify-center space-x-2">
+      <button
+        onClick={() => decreaseQuantity(index)}
+        className="bg-red-500 hover:bg-red-600 text-white px-1 py-1 rounded-lg"
+        disabled={pricing.quantity === 0}
+      >
+        <span style={{ fontSize: '1em' }}>-</span>
+      </button>
+      <span className="text-base">{pricing.quantity}</span>
+      <button
+        onClick={() => increaseQuantity(index)}
+        className="bg-green-500 hover:bg-green-600 text-white px-1 py-1 rounded-lg"
+        disabled={pricing.quantity >= pricing.stock}
+      >
+        <span style={{ fontSize: '1em' }}>+</span>
+      </button>
+    </div>
+    <div className="h-2"></div>
+    <button
+      onClick={() => addToCart(pricing)}
+      className="mt-1 bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded-lg"
+    >
+      Agregar al Carrito
+    </button>
+    <div className="mt-2">
+      <button
+        onClick={() => openPreview(pricing)}
+        className="bg-gray-500 hover:bg-gray-600 text-white px-2 py-1 rounded-lg"
+      >
+        Ver más
+      </button>
+    </div>
+  </div>
+))}
+
           </div>
         )}
       </div>
+
+      {/* Modal de vista previa */}
+      {selectedProduct && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-gray-300 bg-opacity-75">
+    <div className="relative w-4/5 sm:w-2/3 md:w-1/2 bg-white rounded-lg shadow-2xl p-4">
+      <button
+        className="absolute top-2 right-2 text-red-700 hover:text-gray-800"
+        onClick={closePreview}
+      >
+        Cerrar
+      </button>
+      <div className="text-center">
+        <img src={selectedProduct.imageUrl} alt={selectedProduct.name} className="w-48 h-48 mx-auto mb-4 rounded-full" />
+        <h2 className="text-2xl font-semibold text-black mb-2">{selectedProduct.name}</h2>
+        <p className="text-lg text-gray-700 mb-4">${selectedProduct.precio}</p>
+        <ul className="text-sm text-gray-600 space-y-2">
+          <li>Sabor: {selectedProduct.flavor}</li>
+          <li>{selectedProduct.puffs} Puffs</li>
+          <li>{selectedProduct.marca}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+      )}
     </div>
   );
 }
+
 export default Cards;
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import React from 'react';
-
-
+import { Provider } from 'react-redux'; // Importa Provider
+import store from '../Redux/store.js'; // Importa tu tienda Redux
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}> {/* Envuelve tu aplicación con Provider y pasa la tienda Redux */}
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </BrowserRouter>
 );
