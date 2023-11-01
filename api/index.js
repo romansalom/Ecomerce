4cdb0992769a7ebc0b3886551fd4f4fdafcadd5b
romansalom <roman.salom12@gmail.com>
2023-10-31 23:39:55 -0300

    modificando los selectr

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   
   server.listen(3001, async () => {
     console.log('%s listening at 3001, estoy escuchando en el puerto 3001');
