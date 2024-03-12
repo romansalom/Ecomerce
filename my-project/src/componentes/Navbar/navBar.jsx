98855e5675a1874da9491d7f0ae16b503759e1c9
rolo salom <roman.salom12@gmail.com>
2024-03-12 16:30:14 -0300

    agregando carrito de abajo

--- my-project/src/componentes/Navbar/navBar.jsx
+++ my-project/src/componentes/Navbar/navBar.jsx
@@ -11,13 +11,22 @@ import {
   Navbar,
   NavbarContent,
   NavbarItem,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
 } from '@nextui-org/react';
 function Navbars() {
   const [open, setOpen] = useState(true);
   const [inicioSesionModalIsOpen, setInicioSesionModalIsOpen] = useState(false);
   const [registroModalIsOpen, setRegistroModalIsOpen] = useState(false);
   const [usuarioAutenticado, setUsuarioAutenticado] = useState(false);
-  const [carrito, setCarrito] = useState(null); // Agrega este estado
+  const [carrito, setCarrito] = useState(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [modalPlacement] = useState('bottom-center'); // Agrega este estado
   useEffect(() => {
     // Aquí debes poner la lógica para verificar si el usuario está autenticado.
     // Por ejemplo, puedes comprobar si tienes un token de autenticación en el almacenamiento local.
@@ -217,12 +226,99 @@ function Navbars() {
                           </div>
                         </DropdownItem>
                         <DropdownItem>
-                          <span className="flex justify-center px-4 py-2 text-blue-500 cursor-pointer">
-                            Ver Carrito
-                          </span>
+                          <div className="flex flex-col gap-2">
+                            <Button onClick={onOpen} className="max-w-fit">
+                              Ver carrito{' '}
+                            </Button>
+                          </div>
                         </DropdownItem>
                       </DropdownMenu>
                     </Dropdown>
+                    <div>
+                      {' '}
+                      <Modal
+                        isOpen={isOpen}
+                        placement={modalPlacement}
+                        onOpenChange={onOpenChange}
+                      >
+                        <ModalContent>
+                          {(onClose) => (
+                            <>
+                              <ModalHeader className="flex flex-col gap-1">
+                                Modal Title
+                              </ModalHeader>
+                              <ModalBody>
+                                {carrito &&
+                                  carrito.Productos &&
+                                  carrito.Productos.map((product, index) => (
+                                    <div key={index}>
+                                      <div className="flex items-center">
+                                        <div className="img1 mr-4">
+                                          <Image
+                                            src={product.imageUrl}
+                                            alt={product.name}
+                                            height="50px"
+                                            width="50px"
+                                            className="rounded-full border"
+                                          />
+                                        </div>
+                                        <div className="flex flex-col flex-grow">
+                                          <span className="font-custom span1 mb-1">
+                                            {product.name}
+                                          </span>
+                                          <span className="font-custom span2 mb-1">
+                                            {product.puffs} Puffs
+                                          </span>
+                                          <span className="font-custom span2 mb-1">
+                                            {product.CarritoProducto.cantidad}U
+                                          </span>
+                                          <span className="font-custom span2 mb-1">
+                                            ${product.precio}
+                                          </span>
+                                        </div>
+                                        <div className="ml-8">
+                                          <a>
+                                            <svg
+                                              className="w-5 h-5 text-gray-800 dark:text-white"
+                                              aria-hidden="true"
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              fill="blue"
+                                              viewBox="0 0 24 24"
+                                            >
+                                              <path
+                                                fillRule="evenodd"
+                                                d="M11.3 6.2H5a2 2 0 0 0-2 2V19a2 2 0 0 0 2 2h11c1.1 0 2-1 2-2.1V11l-4 4.2c-.3.3-.7.6-1.2.7l-2.7.6c-1.7.3-3.3-1.3-3-3.1l.6-2.9c.1-.5.4-1 .7-1.3l3-3.1Z"
+                                                clipRule="evenodd"
+                                              />
+                                              <path
+                                                fillRule="evenodd"
+                                                d="M19.8 4.3a2.1 2.1 0 0 0-1-1.1 2 2 0 0 0-2.2.4l-.6.6 2.9 3 .5-.6a2.1 2.1 0 0 0 .6-1.5c0-.2 0-.5-.2-.8Zm-2.4 4.4-2.8-3-4.8 5-.1.3-.7 3c0 .3.3.7.6.6l2.7-.6.3-.1 4.7-5Z"
+                                                clipRule="evenodd"
+                                              />
+                                            </svg>
+                                          </a>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  ))}
+                              </ModalBody>
+                              <ModalFooter>
+                                <Button
+                                  color="danger"
+                                  variant="light"
+                                  onPress={onClose}
+                                >
+                                  Close
+                                </Button>
+                                <Button color="primary" onPress={onClose}>
+                                  Action
+                                </Button>
+                              </ModalFooter>
+                            </>
+                          )}
+                        </ModalContent>
+                      </Modal>
+                    </div>
                     <NavbarContent>
                       <NavbarItem>
                         <a
