176430d111a896836d96da74095d2c5d5b2ba0ca
rolo salom <roman.salom12@gmail.com>
2024-03-21 14:07:51 -0300

    agreganDO ESTILOS AL MODAL DE APERTURA DE LA TARJETA

--- my-project/src/componentes/Cards/cards.jsx
+++ my-project/src/componentes/Cards/cards.jsx
@@ -469,6 +469,10 @@ function Cards() {
                   {selectedProduct.marca}
                 </li>
                 <li>
+                  <span className="font-custom">Modelo:</span>{' '}
+                  {selectedProduct.modelo}
+                </li>
+                <li>
                   {mensaje && (
                     <div className="bg-green-500 text-white px-4 py-2 rounded">
                       {mensaje}
