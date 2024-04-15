1da296c99b6f2c3569eacc25c3197bd48900f5bd
rolo salom <roman.salom12@gmail.com>
2024-04-15 11:41:29 -0300

    modificando el navabr

--- my-project/src/componentes/Navbar/navBar.jsx
+++ my-project/src/componentes/Navbar/navBar.jsx
@@ -13,7 +13,6 @@ import {
   NavbarItem,
   Modal,
   ModalContent,
-  ModalFooter,
   Button,
   useDisclosure,
   Avatar,
@@ -271,127 +270,124 @@ function Navbars() {
                         scrollBehavior={'outside'}
                       >
                         <ModalContent>
-                          {(onClose) => (
-                            <>
-                              <section>
-                                <div className=" mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-                                  <div className="mx-auto max-w-3xl">
-                                    <header className="text-center">
-                                      <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
-                                        Mi Carrito
-                                      </h1>
-                                    </header>
+                          <>
+                            <section className="contenedores-zoom">
+                              <div className=" mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                                <div className="mx-auto max-w-3xl">
+                                  <header className="text-center">
+                                    <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
+                                      Mi Carrito
+                                    </h1>
+                                  </header>
 
-                                    <div className="mt-8">
-                                      <ul className="space-y-4">
-                                        <ScrollShadow>
-                                          {carrito &&
-                                            carrito.Productos &&
-                                            carrito.Productos.map(
-                                              (product, index) => (
-                                                <li
-                                                  key={index}
-                                                  className="flex items-center gap-6 mt-3"
-                                                >
-                                                  <Image
-                                                    src={product.imageUrl}
-                                                    alt={product.name}
-                                                    height="50px"
-                                                    width="50px"
-                                                    className="size-16 rounded object-cover"
-                                                  />
+                                  <div className="mt-8">
+                                    <ul className="space-y-4">
+                                      <ScrollShadow>
+                                        {carrito &&
+                                          carrito.Productos &&
+                                          carrito.Productos.map(
+                                            (product, index) => (
+                                              <li
+                                                key={index}
+                                                className="flex items-center gap-6 mt-3"
+                                              >
+                                                <Image
+                                                  src={product.imageUrl}
+                                                  alt={product.name}
+                                                  height="50px"
+                                                  width="50px"
+                                                  className="size-16 rounded object-cover"
+                                                />
 
-                                                  <div>
-                                                    <h3 className="text-sm text-gray-900">
-                                                      {product.name}{' '}
-                                                    </h3>
+                                                <div>
+                                                  <h3 className="text-sm text-gray-900">
+                                                    {product.name}{' '}
+                                                  </h3>
 
-                                                    <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                                                      <div>
-                                                        <dt className="inline">
-                                                          {product.puffs} Puffs
-                                                        </dt>
-                                                      </div>
-                                                      <div>
-                                                        <dt className="inline">
-                                                          {product.flavor}
-                                                        </dt>
-                                                      </div>
-                                                      <div>
-                                                        <dt className="inline">
-                                                          ${product.precio}
-                                                        </dt>
-                                                      </div>
-                                                    </dl>
-                                                  </div>
+                                                  <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
+                                                    <div>
+                                                      <dt className="inline">
+                                                        {product.puffs} Puffs
+                                                      </dt>
+                                                    </div>
+                                                    <div>
+                                                      <dt className="inline">
+                                                        {product.flavor}
+                                                      </dt>
+                                                    </div>
+                                                    <div>
+                                                      <dt className="inline">
+                                                        ${product.precio}
+                                                      </dt>
+                                                    </div>
+                                                  </dl>
+                                                </div>
 
-                                                  <div className="flex flex-1 items-center justify-end gap-2">
-                                                    <span className="sr-only">
-                                                      Remove item
-                                                    </span>
-                                                    <dt className="inline">
-                                                      {
-                                                        product.CarritoProducto
-                                                          .cantidad
-                                                      }
-                                                      U
-                                                    </dt>
-                                                    <svg
-                                                      xmlns="http://www.w3.org/2000/svg"
-                                                      fill="none"
-                                                      viewBox="0 0 24 24"
-                                                      strokeWidth="1.5"
-                                                      stroke="currentColor"
-                                                      className="h-4 w-4"
-                                                    >
-                                                      <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                                      />
-                                                    </svg>
-                                                  </div>
-                                                </li>
-                                              )
-                                            )}
-                                        </ScrollShadow>
-                                      </ul>
-
-                                      <div className=" flex justify-end border-t border-gray-100 pt-8">
-                                        <div className="w-full max-w-lg md:w-screen md:max-w-full space-y-4">
-                                          <dl className="space-y-0.5 text-sm text-gray-700">
-                                            <div className="flex justify-between !text-base font-medium">
-                                              <dt>Total</dt>
-                                              <dd>
-                                                {carrito &&
-                                                carrito.Productos ? (
-                                                  <span className="font-custom">
-                                                    ${totalCarrito.toFixed(2)}{' '}
-                                                    {/* Mostrar el botón Pagar solo en pantallas pequeñas */}
-                                                    <Button
-                                                      color="primary"
-                                                      onClick={PagarButton}
-                                                    >
-                                                      Pagar
-                                                    </Button>
+                                                <div className="flex flex-1 items-center justify-end gap-2">
+                                                  <span className="sr-only">
+                                                    Remove item
                                                   </span>
-                                                ) : (
-                                                  <span className="font-custom">
-                                                    $0.00
-                                                  </span>
-                                                )}
-                                              </dd>
-                                            </div>
-                                          </dl>
-                                          {/* Contenedor para el botón Pagar en pantallas grandes */}
-                                        </div>
+                                                  <dt className="inline">
+                                                    {
+                                                      product.CarritoProducto
+                                                        .cantidad
+                                                    }
+                                                    U
+                                                  </dt>
+                                                  <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth="1.5"
+                                                    stroke="currentColor"
+                                                    className="h-4 w-4"
+                                                  >
+                                                    <path
+                                                      strokeLinecap="round"
+                                                      strokeLinejoin="round"
+                                                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                    />
+                                                  </svg>
+                                                </div>
+                                              </li>
+                                            )
+                                          )}
+                                      </ScrollShadow>
+                                    </ul>
+
+                                    <div className=" flex justify-end border-t border-gray-100 pt-8">
+                                      <div className="w-full max-w-lg md:w-screen md:max-w-full space-y-4">
+                                        <dl className="space-y-0.5 text-sm text-gray-700">
+                                          <div className="flex justify-between !text-base font-medium">
+                                            <dt>Total</dt>
+                                            <dd>
+                                              {carrito && carrito.Productos ? (
+                                                <span className="font-custom">
+                                                  ${totalCarrito.toFixed(2)}{' '}
+                                                  {/* Mostrar el botón Pagar solo en pantallas pequeñas */}
+                                                  <Button
+                                                    color="primary"
+                                                    onClick={PagarButton}
+                                                  >
+                                                    Pagar
+                                                  </Button>
+                                                </span>
+                                              ) : (
+                                                <span className="font-custom">
+                                                  $0.00
+                                                </span>
+                                              )}
+                                            </dd>
+                                          </div>
+                                        </dl>
+                                        {/* Contenedor para el botón Pagar en pantallas grandes */}
                                       </div>
                                     </div>
                                   </div>
                                 </div>
-                              </section>
-                            </>
-                          )}
+                              </div>
+                            </section>
+                          </>
                         </ModalContent>
                       </Modal>
                     </div>
